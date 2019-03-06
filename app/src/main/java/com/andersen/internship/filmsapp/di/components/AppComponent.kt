bacf34751d43ed4d7b11e68c1660c3aa6e406cfb
Pavel1234567 <pavel_azhel@rambler.ru>
2019-03-06 12:08:18 +0300

    добавил app scope

--- app/src/main/java/com/andersen/internship/filmsapp/di/components/AppComponent.kt
+++ app/src/main/java/com/andersen/internship/filmsapp/di/components/AppComponent.kt
@@ -1,13 +1,14 @@
 package com.andersen.internship.filmsapp.di.components
 
 import com.andersen.internship.filmsapp.di.modules.NetworkModule
+import com.andersen.internship.filmsapp.di.scopes.AppScope
 import com.andersen.internship.filmsapp.mvp.models.MainModel
 import com.andersen.internship.filmsapp.mvp.presenters.MainPresenter
 import dagger.Component
 import javax.inject.Singleton
 
 @Component(modules = arrayOf(NetworkModule::class))
-@Singleton
+@AppScope
 interface AppComponent {
 
     fun injectNetworkService(modelFilmsInterface: MainModel)
---
+++ app/src/main/java/com/andersen/internship/filmsapp/di/scopes/AppScope.kt
@@ -0,0 +1,7 @@
+package com.andersen.internship.filmsapp.di.scopes
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AppScope
