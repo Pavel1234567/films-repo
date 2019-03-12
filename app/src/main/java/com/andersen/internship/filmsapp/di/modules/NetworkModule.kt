50df9f19d247d164b20345d28e59aaa6d152fdc7
Pavel1234567 <pavel_azhel@rambler.ru>
2019-03-12 11:24:16 +0300

    убрал класс network service и перенёс его функционал в network module

--- app/src/main/java/com/andersen/internship/filmsapp/di/modules/NetworkModule.kt
+++ app/src/main/java/com/andersen/internship/filmsapp/di/modules/NetworkModule.kt
@@ -1,12 +1,14 @@
 package com.andersen.internship.filmsapp.di.modules
 
-import com.andersen.internship.filmsapp.di.scopes.ActivityScope
-import com.andersen.internship.filmsapp.mvp.contracts.main.ModelFilmsInterface
 import com.andersen.internship.filmsapp.mvp.models.ModelFilmsRepository
-import com.andersen.internship.filmsapp.mvp.presenters.FilmsPresenter
-import com.andersen.internship.filmsapp.network.NetworkService
+import com.andersen.internship.filmsapp.network.api.FilmsApi
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -14,11 +16,23 @@ class NetworkModule {
 
     @Singleton
     @Provides
-    fun networkService(): NetworkService = NetworkService()
+    fun modelForFilms(filmsApi: FilmsApi): ModelFilmsRepository = ModelFilmsRepository(filmsApi)
 
     @Singleton
     @Provides
-    fun modelForFilms(networkService: NetworkService): ModelFilmsRepository = ModelFilmsRepository(networkService)
+    fun filmsApi(retrofit: Retrofit) = retrofit.create(FilmsApi::class.java)
 
+    @Singleton
+    @Provides
+    fun retrofit(gson: Gson) = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
 
+    @Singleton
+    @Provides
+    fun gson() = GsonBuilder().create()
 }
+
+private val BASE_URL = "https://gist.github.com/giffell/"
