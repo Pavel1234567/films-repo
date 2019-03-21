fb659bb1ecc349dcf9ee48ec67058482d69d0bd1
Pavel1234567 <pavel_azhel@rambler.ru>
2019-03-21 13:05:48 +0300

    избавляюсь от ListFilms

--- app/src/main/java/com/andersen/internship/filmsapp/mvp/contracts/main/ModelFilmsInterface.kt
+++ app/src/main/java/com/andersen/internship/filmsapp/mvp/contracts/main/ModelFilmsInterface.kt
@@ -1,10 +1,9 @@
 package com.andersen.internship.filmsapp.mvp.contracts.main
 
-import com.andersen.internship.filmsapp.pojo.films.ListFilms
-import io.reactivex.Observable
+import com.andersen.internship.filmsapp.pojo.films.Film
 import io.reactivex.Single
 
 interface ModelFilmsInterface {
 
-    fun loadFilms(): Single<ListFilms>
+    fun loadFilms(): Single<List<Film>>
 }
--- app/src/main/java/com/andersen/internship/filmsapp/mvp/models/ModelFilmsRepository.kt
+++ app/src/main/java/com/andersen/internship/filmsapp/mvp/models/ModelFilmsRepository.kt
@@ -2,11 +2,11 @@ package com.andersen.internship.filmsapp.mvp.models
 
 import com.andersen.internship.filmsapp.mvp.contracts.main.ModelFilmsInterface
 import com.andersen.internship.filmsapp.network.api.FilmsApi
-import com.andersen.internship.filmsapp.pojo.films.ListFilms
+import com.andersen.internship.filmsapp.pojo.films.Film
 import io.reactivex.Single
 import javax.inject.Inject
 
 class ModelFilmsRepository @Inject constructor(private val filmsApi: FilmsApi): ModelFilmsInterface {
 
-    override fun loadFilms(): Single<ListFilms> = filmsApi.getFilmsList()
+    override fun loadFilms(): Single<List<Film>> = filmsApi.getFilmsList().map { it.films }
 }
