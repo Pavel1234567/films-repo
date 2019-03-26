96d8d8661ab76b02328d499075b0b59e17f3aa90
Pavel1234567 <pavel_azhel@rambler.ru>
2019-03-26 12:29:31 +0300

    .

--- app/src/main/java/com/andersen/internship/filmsapp/ui/adapters/FilterFilms.kt
+++ app/src/main/java/com/andersen/internship/filmsapp/ui/adapters/FilterFilms.kt
@@ -1,4 +1,34 @@
 package com.andersen.internship.filmsapp.ui.adapters
 
-class FilterFilms {
+import android.widget.Filter
+import com.andersen.internship.filmsapp.pojo.films.FilmDTO
+
+class FilterFilms(private val adapter: FilmItemAdapter): Filter() {
+
+    private val fullListFilms = adapter.listFilms.toList()
+
+    override fun performFiltering(constraint: CharSequence?): FilterResults {
+
+        val filteredListFilms = mutableListOf<FilmDTO>()
+
+        if (constraint.isNullOrEmpty()){
+            filteredListFilms.addAll(fullListFilms)
+        }else{
+
+            val filterPattern = constraint.toString().toLowerCase().trim()
+            fullListFilms.forEach {
+                if (it.title.contains(filterPattern)){
+                    filteredListFilms.add(it)
+                }
+            }
+        }
+
+        val results = FilterResults()
+        results.values = filteredListFilms
+        return results
+    }
+
+    override fun publishResults(charSequence: CharSequence?, filterResults: FilterResults?) {
+        adapter.listFilms = filterResults?.values as List<FilmDTO>
+    }
 }
